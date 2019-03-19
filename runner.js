54859d07e7ba4ab4f27c5c880ca569019d32d90a
Daniil <kadaniilo@gmail.com>
2019-03-19 12:17:40 +0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -6,32 +6,32 @@ let orange = new orangeModule();
 let tree = new orangeTreeModule();
 console.log(orange)
 
-// # Let seasons pass until the tree is ready to bear fruit.
-while(!tree.isMature) {
-  tree.passGrowingSeason;
+// Let seasons pass until the tree is ready to bear fruit.
+while(!tree.isMature()) {
+  tree.passGrowingSeason();
 }
 
 
 // # Report yearly harvest information for the lifetime of the tree.
-while(!tree.isdead){
-  tree.passGrowingSeason;
+while(!tree.isDead()){
+  tree.passGrowingSeason();
   harvestedOranges = [];
 
-  while(tree.hasOranges) {
-    harvestedOranges.push(tree.pickAnOrange)
+  while(tree.hasOranges()) {
+    harvestedOranges.push(tree.pickAnOrange())
   }
 
  // average_orange_diameter = # Need to calculate the average diameter for this harvest.
 
   console.log(
-    ```
+    `
     HARVEST_REPORT
     YEAR ${tree.age} REPORT
     -----------------------
     Height: ${tree.height} feet.
     Harvest: ${harvestedOranges.length} oranges with an average
     diameter of ${averageOrangeDiameter} inches.
-    ```
+    `
   )
 }
 
