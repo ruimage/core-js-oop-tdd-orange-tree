ec9b8daf4e34684ed9e623448e0b784de35f49b2
Anton Valkovsky <ruizave@gmail.com>
2022-05-24 15:20:03 +0300

    отформатирован код

--- orange-tree.js
+++ orange-tree.js
@@ -1,7 +1,7 @@
-const Orange = require("./orange");
+const Orange = require('./orange');
 
 class OrangeTree {
-  constructor(age, height){
+  constructor(age, height) {
     this.age = age;
     this.height = height;
     this.orangesOnTree = [];
@@ -9,29 +9,28 @@ class OrangeTree {
 
   passGrowingSeason() {
     this.orangesOnTree.length = 0;
-    if(this.isDead()){
-      return "Tree is dead";
+    if (this.isDead()) {
+      return 'Tree is dead';
     }
     this.age += 1;
-    if(this.height <= 25){
+    if (this.height <= 25) {
       this.height += 2.5;
     }
-    if(this.isMature()){
+    if (this.isMature()) {
       let count = Math.floor(100 + Math.random() * 200);
-      while(count > 0){
+      while (count > 0) {
         this.orangesOnTree.push(new Orange());
         count--;
       }
     }
   }
 
-
   isMature() {
     //  Returns true if the tree is old enough to bear fruit, false otherwise
     return this.age >= 6;
   }
 
-  isDead(){
+  isDead() {
     return this.age >= 100;
   }
 
@@ -45,22 +44,11 @@ class OrangeTree {
     //  Raises a NoOrangesError otherwise
     if (!this.hasOranges()) {
       throw Error('This tree has no oranges');
-    }
-    else{
+    } else {
       return this.orangesOnTree.pop();
     }
     //  orange-picking logic goes here
   }
 }
-
-let tree = new OrangeTree(99, 10);
-tree.passGrowingSeason();
-// console.log(tree.orangesOnTree);
-// console.log(tree.age);
-// console.log(tree.height);
-// console.log(tree.hasOranges());
-console.log(tree);
-console.log(tree.passGrowingSeason());
-console.log(tree);
 
 module.exports = OrangeTree;
